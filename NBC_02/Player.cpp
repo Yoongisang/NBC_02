67ebbd713bb9d020bbd201e640583af496b32bcf
Yoongisang <sawsd@naver.com>
2025-12-15 18:25:11 +0900

    Player class

---
+++ NBC_02/Player.cpp
@@ -0,0 +1,23 @@
+﻿#include "Player.h"
+#include <iostream>
+
+Player::Player(string nickname)
+{
+    setNickname(nickname);
+}
+
+
+void Player::printPlayerStatus()
+{
+    cout << "------------------------------------" << '\n';
+    cout << "* 현재 능력치" << '\n';
+    cout << "닉네임: " << nickname << '\n';
+    cout << "Lv. " << level << '\n';
+    cout << "HP: " << HP << '\n';
+    cout << "MP: " << MP << '\n';
+    cout << "공격력: " << power << '\n';
+    cout << "방어력: " << defence << '\n';
+    cout << "정확도: " << accuracy << '\n';
+    cout << "속도: " << speed << '\n';
+    cout << "------------------------------------" << '\n';
+}
---
+++ NBC_02/Player.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <string>
+
+using namespace std;
+
+class Player {
+public:
+    Player(string nickname);
+
+    virtual void attack() = 0;
+    void printPlayerStatus();
+
+    // getter 함수
+    string getJobName() const { return job_name; }
+    string getNickname() const { return nickname; }
+    int getLevel() const { return level; }
+    int getHP() const { return HP; }
+    int getMP() const { return MP; }
+    int getPower() const { return power; }
+    int getDefence() const { return defence; }
+    int getAccuracy() const { return accuracy; }
+    int getSpeed() const { return speed; }
+
+    // setter 함수
+    void setNickname(string nickname) { this->nickname = nickname; }
+    void setHP(int HP) { this->HP = HP; }
+    void setMP(int MP) { this->MP = MP; }
+    void setPower(int power) { this->power = power; }
+    void setDefence(int defence) { this->defence = defence; }
+    void setAccuracy(int accuracy) { this->accuracy = accuracy; }
+    void setSpeed(int speed) { this->speed = speed; }
+
+protected:
+    string job_name;
+    string nickname;
+    int level;
+    int HP;
+    int MP;
+    int power;
+    int defence;
+    int accuracy;
+    int speed;
+};
