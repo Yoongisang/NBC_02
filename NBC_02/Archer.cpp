65a039a0cb1daee4c47b036f5ae009f3e03bec15
Yoongisang <sawsd@naver.com>
2025-12-15 21:09:52 +0900

    Update Archer.cpp

--- NBC_02/Archer.cpp
+++ NBC_02/Archer.cpp
@@ -3,8 +3,17 @@
 
 Archer::Archer(string nickname) : Player(nickname)
 {
+	job_name = "궁수";
+	level = 5;
+	HP = 100;
+	MP = 100;
+	power = 50;
+	defence = 10;
+	accuracy = 80;
+	speed = 50;
 }
 
 void Archer::attack()
 {
+	cout << "궁수 공격: " << power << '\n';
 }
